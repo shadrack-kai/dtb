fd614baf492f3bf6e4106c53236c4675d61bb735
SKai <shadrak.kai@gmail.com>
2025-05-07 19:26:11 +0300

    Updated profile service

---
+++ profile-service/src/main/java/com/dtb/user/CustomerProfileServiceApplication.java
@@ -0,0 +1,13 @@
+package com.dtb.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerProfileServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CustomerProfileServiceApplication.class, args);
+    }
+
+}
